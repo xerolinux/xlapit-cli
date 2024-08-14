48f562e95d9b3234e2f0a04a6b8442b93cc73dae
DarkXero-dev <steve@techxero.com>
2024-08-14 15:31:20 +0300

    Tailscale

--- xero-scripts/scripts/drivers.sh
+++ xero-scripts/scripts/drivers.sh
@@ -31,7 +31,8 @@ display_options() {
   gum style --foreground 35 "2. Printer Drivers and Tools."
   gum style --foreground 35 "3. Samba Tools (XeroLinux Repo)."
   gum style --foreground 35 "4. Scanner Drivers (XeroLinux Repo)."
-  gum style --foreground 35 "5. DeckLink & StreamDeck Drivers/Tools (AUR)."
+  gum style --foreground 35 "5. Install Tailscale w/XeroLinux fix."
+  gum style --foreground 35 "6. DeckLink & StreamDeck Drivers/Tools (AUR)."
   echo
   gum style --foreground 196 "k. Install CachyOS Kernel Manager (CashyOS Repos)."
   echo
@@ -206,6 +207,17 @@ process_choice() {
         clear && exec "$0"
         ;;
       5)
+        gum style --foreground 35 "Installing Tailscale..."
+        sleep 2
+        echo
+        cd ~ && wget https://raw.githubusercontent.com/xerolinux/xero-fixes/main/conf/install.sh
+        chmod +x ~/install.sh && sh install.sh
+        rm ~/install.sh
+        gum style --foreground 35 "Tailscale installation complete!"
+        sleep 3
+        clear && exec "$0"
+        ;;
+      6)
         gum style --foreground 35 "Installing DeckLink & StreamDeck Drivers/Tools..."
         sleep 2
         echo
