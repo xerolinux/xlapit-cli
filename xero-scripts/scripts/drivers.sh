5424b0bf9657fae6cffe5c3c1944172aa6824556
DarkXero-dev <steve@techxero.com>
2024-09-02 21:40:26 +0300

    Fixed nvidia

--- xero-scripts/scripts/drivers.sh
+++ xero-scripts/scripts/drivers.sh
@@ -107,7 +107,7 @@ prompt_user() {
         read -rp "Older 900/1000 series or Newer 1650ti/1660ti/20 series and up? (o/n): " nvidia_series
         if [[ $nvidia_series == "o" || $nvidia_series == "1000" ]]; then
           sudo pacman -S --needed --noconfirm linux-headers nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader egl-wayland opencl-nvidia lib32-opencl-nvidia libvdpau-va-gl libvdpau
-        elif [[ $nvidia_series == "d" ]]; then
+        elif [[ $nvidia_series == "n" ]]; then
           sudo pacman -S --needed --noconfirm linux-headers nvidia-open-dkms nvidia-utils lib32-nvidia-utils nvidia-settings vulkan-icd-loader lib32-vulkan-icd-loader egl-wayland opencl-nvidia lib32-opencl-nvidia libvdpau-va-gl libvdpau
         else
           echo "Invalid selection."
