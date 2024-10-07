98272672918ff8f82367e2d95afa2dd22b73289f
DarkXero-dev <steve@techxero.com>
2024-10-07 11:56:57 +0300

    nVidia

--- xero-scripts/scripts/drivers.sh
+++ xero-scripts/scripts/drivers.sh
@@ -101,9 +101,8 @@ prompt_user() {
         fi
         # Insert these commands before CUDA installation
         sudo sed -i '/^MODULES=(/ s/)$/nvidia nvidia_modeset nvidia_uvm nvidia_drm)/' /etc/mkinitcpio.conf
-        sudo systemctl enable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service nvidia-powerd.service
-        echo -e 'options nvidia NVreg_UsePageAttributeTable=1 NVreg_InitializeSystemMemoryAllocations=0 NVreg_DynamicPowerManagement=0x02' | sudo tee -a /etc/modprobe.d/nvidia.conf
-        echo -e 'options nvidia_drm modeset=1 fbdev=1' | sudo tee -a /etc/modprobe.d/nvidia.conf && sudo mkinitcpio -P
+        sudo systemctl enable nvidia-suspend.service nvidia-hibernate.service nvidia-resume.service
+        echo -e 'options nvidia NVreg_PreserveVideoMemoryAllocations=1 NVreg_TemporaryFilePath=/var/tmp' | sudo tee -a /etc/modprobe.d/nvidia-sleep.conf
         echo
         read -rp "Do you want to install CUDA for Machine Learning? (y/n): " cuda
         if [[ $cuda =~ ^[Yy](es)?$ ]]; then
